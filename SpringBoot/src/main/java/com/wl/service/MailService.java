f4405be4a2d25d9f3458a25a3bf9d461184de8ff
wulang <wul@koal.com>
2019-05-22 16:15:42 +0800

    发送邮件

--- SpringBoot/src/main/java/com/wl/service/MailService.java
+++ SpringBoot/src/main/java/com/wl/service/MailService.java
@@ -5,4 +5,7 @@ public interface MailService {
 
     public void sendSimpleMail(String to, String subject, String content);
 
+    public void sendAttachmentsMail(String to, String subject, String content, String filePath);
+
+    public void sendOnlineResourceMail(String to, String subject, String content, String resourcePath, String id);
 }
--- SpringBoot/src/test/wul/MailTest.java
+++ SpringBoot/src/test/wul/MailTest.java
@@ -8,6 +8,8 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 import javax.annotation.Resource;
 
@@ -18,8 +20,33 @@ public class MailTest {
     @Resource
     private MailServiceImpl mailService;
 
+    @Autowired
+    private TemplateEngine templateEngine;
+
     @Test
     public void simpleMail(){
         mailService.sendSimpleMail("wul@koal.com", "test simple mail", "hello this is simple mail");
+    }
+
+    @Test
+    public void sendAttachmentsMail(){
+        String filePath = "D:\\HexTest.java";
+        mailService.sendAttachmentsMail("wul@koal.com", "test simple mail", "hello this is Attachment mail", filePath);
+    }
+
+    @Test
+    public void sendInlineMail(){
+        String rscId = "neo006";
+        String content = "<html><body><img src=\'cid:"+rscId+"\'/></body></html>";
+        String path = "C:\\Users\\admin\\Pictures\\Camera Roll\\1556163835447.jpeg";
+        mailService.sendOnlineResourceMail("wul@koal.com", "test simple mail", content, path, rscId);
+    }
+
+    @Test
+    public void sendTemplateMail(){
+        Context context = new Context();
+        context.setVariable("id", "006");
+        String emailTemplate = templateEngine.process("emailTemplate", context);
+        mailService.sendSimpleMail("wul@koal.com", "这是模板邮件", emailTemplate);
     }
 }
