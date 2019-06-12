21c6153416259b1cb0f9233316b349d03f41c0e1
吴浪 <wul@koal.com>
2019-06-12 14:25:50 +0800

    springboot-validate 学习springboot数据项校验

---
+++ springboot-validate/src/main/java/com/model/User.java
@@ -0,0 +1,54 @@
+package com.model;
+
+import javax.validation.constraints.*;
+import java.io.Serializable;
+
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @NotEmpty(message = "姓名不能为空")
+    @Size(min = 2, max = 4, message = "姓名长度必须大于 2 且小于 4 字")
+    private String name;
+
+    @NotNull(message = "年龄不能为空")
+    @Min(value = 0, message = "年龄不能小于零")
+    @Max(value = 200, message = "年龄不得大于200岁")
+    private int age;
+
+    @NotNull(message = "生日不能为空")
+    private String birthday;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday='" + birthday + '\'' +
+                '}';
+    }
+}
